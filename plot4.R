10d8d9f10436af1428b7f2c3b3c0ad1ee4c4ef48
Tommi Liittokivi <tliittok@gmail.com>
2015-04-12 21:46:56 +0300

    plot 4

--- plot4.R
+++ plot4.R
@@ -1,16 +1,26 @@
 # get content
 source("setup.R")
 
-# create plot
+# create 2x2 plots
+par(mfcol = c(2,2))
+
+# add plot1
+plot(data$dt, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+# add plot2
 with (data, {
     plot(dt, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
     lines(dt, Sub_metering_2, type="l", col="Red")
     lines(dt, Sub_metering_3, type="l", col="Blue")
 })
-
-# add legend
 legend("topright", col=c("black", "red", "blue"), lty=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+# add plot3
+plot(data$dt, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+# add plot4
+plot(data$dt, data$Global_reactive_power, type="l", xlab="datetime", ylab="Voltage")
+
 # write output file (NOTE! weekdays are locale specific. For generated images they are: to=thu, pe=fri, la=sat)
-dev.copy(png, "plot3.png", height=480, width=480)
+dev.copy(png, "plot4.png", height=480, width=480)
 dev.off()
